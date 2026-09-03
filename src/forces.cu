46dcbbe6a386053e529ff891b46ef639f68960ad
Karthik Nair <karthiknair614@gmail.com>
2026-09-03 21:36:39 +0530

    Added Potential Energy, and get an insanely low 10^-16 Relative Energy Fluctuation now

--- src/forces.cu
+++ src/forces.cu
@@ -4,19 +4,27 @@
 
 __global__
 void compute_forces_kernel(const double* pos_x, const double* pos_y, const double* pos_z,
-                          double* acc_x, double* acc_y, double* acc_z,
+                          double* acc_x, double* acc_y, double* acc_z, double* d_potential_energy,
                           int N, double box, double box_r) {
+  
   int i = blockIdx.x * blockDim.x + threadIdx.x;
 
   if (i >= N) return;
+
+  const double rc2 = 6.25;              // 2.5^2
+  const double inv_rc2 = 1.0 / rc2;
+  const double inv_rc6 = inv_rc2 * inv_rc2 * inv_rc2;
+  double U_rc = 4.0 * inv_rc6 * (inv_rc6 - 1.0);
   
-  float my_x = pos_x[i];
-  float my_y = pos_y[i];
+  double my_x = pos_x[i];
+  double my_y = pos_y[i];
   double my_z = pos_z[i];
 
-  double fx = 0.0f;
-  double fy = 0.0f;
-  double fz = 0.0f;
+  double fx = 0.0;
+  double fy = 0.0;
+  double fz = 0.0;
+
+  double U = 0.0;
 
   for (int j = 0; j < N; j++) {
 
@@ -26,19 +34,20 @@ void compute_forces_kernel(const double* pos_x, const double* pos_y, const doubl
     double dy = my_y - pos_y[j];
     double dz = my_z - pos_z[j];
 
-    dx -= box * roundf(dx * box_r);
-    dy -= box * roundf(dy * box_r);
-    dz -= box * roundf(dz * box_r);
+    dx -= box * round(dx * box_r);
+    dy -= box * round(dy * box_r);
+    dz -= box * round(dz * box_r);
 
     double r2 = dx*dx + dy*dy + dz*dz;
 
-    if (r2 < 1e-12f || r2 > 6.25f) continue; // 2.5^2 = 6.25
+    if (r2 < 1e-12 || r2 > 6.25) continue; // 2.5^2 = 6.25
 
-    double inv_r2 = 1.0f / r2;
+    double inv_r2 = 1.0 / r2;
     double inv_r6 = inv_r2 * inv_r2 * inv_r2;
 
     // Lennard-Jones Force magnitude
-    double f_mag = 24.0f * inv_r2 * inv_r6 * (2.0f * inv_r6 - 1.0f);
+    double f_mag = 24.0 * inv_r2 * inv_r6 * (2.0 * inv_r6 - 1.0);
+    U += 0.5 * (4.0 * inv_r6 * (inv_r6 - 1.0) - U_rc);
 
     fx += dx * f_mag;
     fy += dy * f_mag;
@@ -48,6 +57,7 @@ void compute_forces_kernel(const double* pos_x, const double* pos_y, const doubl
   acc_x[i] = fx;
   acc_y[i] = fy;
   acc_z[i] = fz;
+  atomicAdd(d_potential_energy, U);
 }
 // Computes all forces
 
@@ -57,6 +67,7 @@ compute_all_forces(const std::vector<Particle> &Particles,
                    int nx, double cell_size, double box) {
    int N = Particles.size();
    double box_r = 1 / box; 
+   double potential_energy = 0;
 
    // 1. Convert AoS to SoA (C++ Structs to Flat Arrays)
     std::vector<double> h_pos_x(N), h_pos_y(N), h_pos_z(N);
@@ -69,10 +80,12 @@ compute_all_forces(const std::vector<Particle> &Particles,
     // 2. Allocate GPU Memory (Device)
     double *d_pos_x, *d_pos_y, *d_pos_z;
     double *d_acc_x, *d_acc_y, *d_acc_z;
+    double *d_potential_energy;
     size_t bytes = N * sizeof(double);
     
     cudaMalloc(&d_pos_x, bytes); cudaMalloc(&d_pos_y, bytes); cudaMalloc(&d_pos_z, bytes);
     cudaMalloc(&d_acc_x, bytes); cudaMalloc(&d_acc_y, bytes); cudaMalloc(&d_acc_z, bytes);
+    cudaMalloc(&d_potential_energy, sizeof(double));
 
     // 3. Copy Data to GPU
     cudaMemcpy(d_pos_x, h_pos_x.data(), bytes, cudaMemcpyHostToDevice);
@@ -80,11 +93,12 @@ compute_all_forces(const std::vector<Particle> &Particles,
     cudaMemcpy(d_pos_z, h_pos_z.data(), bytes, cudaMemcpyHostToDevice);
     // Zero out the acceleration arrays on the GPU
     cudaMemset(d_acc_x, 0, bytes); cudaMemset(d_acc_y, 0, bytes); cudaMemset(d_acc_z, 0, bytes);
-
+    cudaMemset(d_potential_energy, 0, sizeof(double));
+    
     // 4. Launch Kernel
     int threads = 256;
     int blocks = (N + threads - 1) / threads; // Ceiling division
-    compute_forces_kernel<<<blocks, threads>>>(d_pos_x, d_pos_y, d_pos_z, d_acc_x, d_acc_y, d_acc_z, N, box, box_r);
+    compute_forces_kernel<<<blocks, threads>>>(d_pos_x, d_pos_y, d_pos_z, d_acc_x, d_acc_y, d_acc_z, d_potential_energy, N, box, box_r);
     
     // Wait for GPU to finish (Good for debugging)
     cudaDeviceSynchronize(); 
@@ -94,10 +108,12 @@ compute_all_forces(const std::vector<Particle> &Particles,
     cudaMemcpy(h_acc_x.data(), d_acc_x, bytes, cudaMemcpyDeviceToHost);
     cudaMemcpy(h_acc_y.data(), d_acc_y, bytes, cudaMemcpyDeviceToHost);
     cudaMemcpy(h_acc_z.data(), d_acc_z, bytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(&potential_energy, d_potential_energy, sizeof(double), cudaMemcpyDeviceToHost);
 
     // 6. Free GPU Memory
     cudaFree(d_pos_x); cudaFree(d_pos_y); cudaFree(d_pos_z);
     cudaFree(d_acc_x); cudaFree(d_acc_y); cudaFree(d_acc_z);
+    cudaFree(d_potential_energy);
 
     // 7. Repack into C++ Vector and Return
     std::vector<Vec3> all_acc(N);
@@ -105,6 +121,6 @@ compute_all_forces(const std::vector<Particle> &Particles,
         all_acc[i] = {h_acc_x[i], h_acc_y[i], h_acc_z[i]};
     }
 
-    return {all_acc, 0.0}; 
+    return {all_acc, potential_energy}; 
    }
 
